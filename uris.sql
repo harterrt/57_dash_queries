92a31cfd5f6b0f9153d5d7e6ac05246557ed1f4d
Ryan Harter <harterrt@gmail.com>
2017-11-02 17:16:12 -0400

    Format uris query

--- uris.sql
+++ uris.sql
@@ -1,28 +1,55 @@
-with a as (
-select client_id, submission_date_s3, sum(scalar_parent_browser_engagement_total_uri_count) as uri
-from main_summary
-where app_name='Firefox'
-and normalized_channel='release'
-and substring(app_version,1,2)='56'
-and submission_date_s3 >= '20170925'
-and subsession_length<=86400 and  subsession_length>=0 and active_ticks>=0
-and sample_id='42'
-group by 1,2
+WITH a AS (
+    SELECT
+        client_id,
+        submission_date_s3,
+        SUM(scalar_parent_browser_engagement_total_uri_count) AS uri
+    FROM main_summary
+    WHERE app_name = 'Firefox'
+      AND normalized_channel = 'release'
+      AND SUBSTRING(app_version, 1, 2) = '56'
+      AND submission_date_s3 >= '20170925'
+      AND subsession_length <= 86400 
+      AND subsession_length >= 0 
+      AND active_ticks >= 0
+      AND sample_id = '42'
+GROUP BY 1, 2
 ),
-b as (
-select submission_date_s3 as date, avg(uri) as uri56 from a group by 1 order by  1
+b AS (
+    SELECT 
+        submission_date_s3 AS date,
+        AVG(uri) as uri56
+    FROM a 
+    GROUP BY 1 
+    ORDER BY 1
 ),
-c as (
-select client_id, submission_date_s3, sum(scalar_parent_browser_engagement_total_uri_count) as uri
-from main_summary
-where app_name='Firefox'
-and submission_date_s3 >= '20170925'
-and normalized_channel='release'
-and subsession_length<=86400 and  subsession_length>=0 and active_ticks>=0
-and sample_id='42'
-group by 1,2
+c AS (
+    SELECT 
+        client_id, 
+        submission_date_s3, 
+        SUM(scalar_parent_browser_engagement_total_uri_count) AS uri
+    FROM main_summary
+    WHERE app_name = 'Firefox'
+      AND submission_date_s3 >= '20170925'
+      AND normalized_channel = 'release'
+      AND subsession_length <= 86400 
+      AND subsession_length >= 0 
+      AND active_ticks >= 0
+      AND sample_id = '42'
+    GROUP BY 1, 2
 ),
-d as (select submission_date_s3 as date, avg(uri)  uriAll from c group by 1 order by 1)
-select
-d.date, uriAll, uri56 from b join d on b.date=d.date
-order by 1
+d AS (
+    SELECT 
+        submission_date_s3 AS date,
+        AVG(uri) AS uriAll
+    FROM c
+    GROUP BY 1 
+    ORDER BY 1
+)
+
+SELECT
+    d.date,
+    uriAll,
+    uri56 
+FROM b
+JOIN d ON b.date=d.date
+ORDER BY 1
