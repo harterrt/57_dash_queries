4ea3bee215a559fb7c039dbb5a84f3474bbf8813
Ryan Harter <harterrt@gmail.com>
2017-11-02 16:59:20 -0400

    Refactor activeticks to dry up code

--- activeticks.sql
+++ activeticks.sql
@@ -7,65 +7,55 @@ TODO:
  - Fix app version filter
  - Remove sampling
  - Fix column names (s/56/57/g)
+Done:
+ - change column names
 */
 
-WITH a AS (
+WITH filtered_data AS (
     SELECT 
         client_id,
         submission_date_s3,
-        SUM(subsession_length) / 3600 AS thours,
-        SUM(active_ticks) * 5 / 3600 AS ahours
+        SUBSTRING(app_version, 1, 2) = '56' AS is_new_version,
+        subsession_length AS subsession_hours,
+        active_ticks AS active_hours
     FROM main_summary
     WHERE app_name = 'Firefox'
-        AND normalized_channel =' release'
-        AND SUBSTRING(app_version, 1, 2) = '56'
+        AND normalized_channel = 'release'
         AND submission_date_s3 >= '20170925'
         AND subsession_length <= 86400
         AND subsession_length >= 0
         AND active_ticks>=0
         AND sample_id='42'
+),
+client_data AS (
+    SELECT
+        client_id,
+        submission_date_s3,
+        SUM(subsession_hours) / 3600 as subsession_hours_all,
+        SUM(active_hours) * 5 / 3600 as active_hours_all,
+        SUM(IF(is_new_version, subsession_hours, NULL)) / 3600  as subsession_hours_new,
+        SUM(IF(is_new_version, active_hours, NULL)) * 5 / 3600 as active_hours_new
+    FROM filtered_data
     GROUP BY 1, 2
 ),
-b AS (
+daily_data AS (
     SELECT
         submission_date_s3 AS date,
-        AVG(thours) AS thrs56,
-        AVG(ahours) AS ahrs56
-    FROM a
+        AVG(subsession_hours_new) AS thours56,
+        AVG(active_hours_new) AS ahours56,
+        AVG(subsession_hours_all) AS thoursAll,
+        AVG(active_hours_all) AS ahoursAll
+    FROM client_data
     GROUP BY 1
-    ORDER BY 1
-),
-c AS (
-SELECT
-    client_id,
-    submission_date_s3,
-    SUM(subsession_length) / 3600 AS thours,
-    SUM(active_ticks) * 5 / 3600 as ahours
-FROM main_summary
-WHERE app_name = 'Firefox'
-    AND submission_date_s3 >= '20170925'
-    AND normalized_channel = 'release'
-    AND subsession_length <= 86400
-    AND subsession_length >= 0
-    AND active_ticks >= 0
-    AND sample_id = '42'
-GROUP BY 1, 2
-),
-d AS (
-    SELECT
-        submission_date_s3 AS date,
-        AVG(thours) as thrsAll,
-        AVG(ahours) as ahrsAll
-    FROM c GROUP BY 1
     ORDER BY 1
 )
 
 SELECT
-    d.date,
-    thrsAll,
-    thrs56,
-    ahrsAll,
-    ahrs56
-FROM b
-JOIN d ON b.date = d.date
+    date,
+    thoursAll,
+    thours56,
+    ahoursAll,
+    ahours56
+FROM daily_data
 ORDER BY 1
+
