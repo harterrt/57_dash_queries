3db303b8b2d79eaec43c9fb7879d3ded8b273ccf
Ryan Harter <harterrt@gmail.com>
2017-11-02 17:26:23 -0400

    Refactor URI's query

--- uris.sql
+++ uris.sql
@@ -1,55 +1,40 @@
-WITH a AS (
+WITH filtered_data AS (
+    SELECT 
+        client_id,
+        submission_date_s3,
+        SUBSTRING(app_version, 1, 2) = '56' AS is_new_version,
+        scalar_parent_browser_engagement_total_uri_count AS uri
+    FROM main_summary
+    WHERE app_name = 'Firefox'
+        AND normalized_channel = 'release'
+        AND submission_date_s3 >= '20170925'
+        AND subsession_length <= 86400
+        AND subsession_length >= 0
+        AND active_ticks>=0
+        AND sample_id='42'
+),
+client_data AS (
     SELECT
         client_id,
         submission_date_s3,
-        SUM(scalar_parent_browser_engagement_total_uri_count) AS uri
-    FROM main_summary
-    WHERE app_name = 'Firefox'
-      AND normalized_channel = 'release'
-      AND SUBSTRING(app_version, 1, 2) = '56'
-      AND submission_date_s3 >= '20170925'
-      AND subsession_length <= 86400 
-      AND subsession_length >= 0 
-      AND active_ticks >= 0
-      AND sample_id = '42'
-GROUP BY 1, 2
-),
-b AS (
-    SELECT 
-        submission_date_s3 AS date,
-        AVG(uri) as uri56
-    FROM a 
-    GROUP BY 1 
-    ORDER BY 1
-),
-c AS (
-    SELECT 
-        client_id, 
-        submission_date_s3, 
-        SUM(scalar_parent_browser_engagement_total_uri_count) AS uri
-    FROM main_summary
-    WHERE app_name = 'Firefox'
-      AND submission_date_s3 >= '20170925'
-      AND normalized_channel = 'release'
-      AND subsession_length <= 86400 
-      AND subsession_length >= 0 
-      AND active_ticks >= 0
-      AND sample_id = '42'
+        SUM(uri) as uri_all,
+        SUM(IF(is_new_version, uri, NULL)) as uri_new
+    FROM filtered_data
     GROUP BY 1, 2
 ),
-d AS (
-    SELECT 
+daily_data AS (
+    SELECT
         submission_date_s3 AS date,
-        AVG(uri) AS uriAll
-    FROM c
-    GROUP BY 1 
+        AVG(uri_new) AS avg_uri_new,
+        AVG(uri_all) AS avg_uri_all
+    FROM client_data
+    GROUP BY 1
     ORDER BY 1
 )
 
 SELECT
-    d.date,
-    uriAll,
-    uri56 
-FROM b
-JOIN d ON b.date=d.date
+    date,
+    avg_uri_new,
+    avg_uri_all
+FROM daily_data
 ORDER BY 1
