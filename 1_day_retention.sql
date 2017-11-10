0b3dedbeaf7977c6148bf42904a5af138c5ed9f0
Ryan Harter <harterrt@gmail.com>
2017-11-10 16:27:40 -0500

    Some readability and safety improvements 1day ret

--- 1_day_retention.sql
+++ 1_day_retention.sql
@@ -1,30 +1,37 @@
- WITH population AS
-  (SELECT subsession_start,
-          merge(cast(hll AS HLL)) AS hll,
-          days_since_creation
-   FROM retention
-   WHERE days_since_creation >= 0
-     AND days_since_creation <= 14
-     AND days_since_creation < date_diff('day', date '2017-06-26', CURRENT_DATE)
-     AND channel = 'release'
-   GROUP BY 1,
-            3),
-      -- The count of profiles broken down by subsession_start and and days relative to the start
+WITH filtered AS (
+    SELECT 
+        subsession_start,
+        days_since_creation
+        merge(cast(hll AS HLL)) AS hll,
+    FROM retention
+    WHERE days_since_creation >= 0
+      AND days_since_creation <= 14
+      AND days_since_creation < date_diff('day', date '2017-06-26', CURRENT_DATE)
+      AND channel = 'release'
+),
+daily_retention AS (
+    SELECT 
+        subsession_start,
+        days_since_creation
+        merge(cast(hll AS HLL)) AS hll,
+    FROM filtered
+    GROUP BY 1, 2
+), -- The count of profiles broken down by subsession_start and and days relative to the start
+daily_new_profiles AS (
+    SELECT 
+        subsession_start,
+        cardinality(merge(hll)) AS total
+    FROM population
+    GROUP BY 1
+) -- The total count of first session profiles for each date
 
-      population_agg AS
-  (SELECT subsession_start,
-          cardinality(merge(hll)) AS total
-   FROM population
-   WHERE days_since_creation = 0
-   GROUP BY 1) -- The total count of first session profiles for each date
-
-SELECT population.subsession_start AS date,
-       days_since_creation AS day_number,
-       cardinality(merge(hll)) AS value,
-       total
-FROM population
-JOIN population_agg ON population.subsession_start = population_agg.subsession_start
-WHERE days_since_creation > 0
-GROUP BY 1,
-         2,
-         4
+SELECT 
+    population.subsession_start AS date,
+    days_since_creation AS day_number,
+    cardinality(merge(hll)) AS value,
+    total
+FROM daily_retention
+JOIN daily_new_profiles 
+  ON daily_retention.subsession_start = daily_new_profiles.subsession_start
+WHERE days_since_creation >= 0
+GROUP BY 1, 2, 4
